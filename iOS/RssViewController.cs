0e601448225199e8f3eddfee88f2aa6833d73ad4
florentinMac <florentin.gonthier@gmail.com>
2018-02-13 12:00:23 +0100

    passage au design cshtml via windows

--- iOS/RssViewController.cs
+++ iOS/RssViewController.cs
@@ -3,6 +3,7 @@ using System;
 using UIKit;
 using NativeRssFeed.Models;
 using NativeRssFeed.Services;
+using NativeRssFeed.iOS.View;
 
 namespace NativeRssFeed.iOS
 {
@@ -11,6 +12,7 @@ namespace NativeRssFeed.iOS
         public string Url { get; set; }
         public rssChannel Channel { get; set; }
         public RssService RssService { get; set; }
+        UIWebView webView;
 
         public RssViewController (IntPtr handle) : base (handle)
         {
@@ -24,6 +26,16 @@ namespace NativeRssFeed.iOS
             if (!string.IsNullOrEmpty(this.Url))
             {
                 this.Channel = this.RssService.GetChannelFromUrl(this.Url);
+                webView = new UIWebView(UIScreen.MainScreen.Bounds);
+                View.Add(webView);
+
+                // Intercept URL loading to handle native calls from browser
+
+                // Render the view from the type generated from RazorView.cshtml
+
+                var template = new DisplayRssChannel() { Model = this.Channel };
+                var page = template.GenerateString();
+                webView.LoadHtmlString(page, NSBundle.MainBundle.BundleUrl);
             }
         }
 
